04bc4d05c724afad268105b52473a4cbecde69b8
andreFSilva <andrealekhine@gmail.com>
2019-11-21 12:47:04 -0300

    Atualizando o requirements

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    produto_novo = models.CharField(max_length=100, unique=True)
+    produto_usado = models.CharField(max_length=100, unique=True)
+    preco = models.DecimalField('preço',max_digits=7, decimal_places=2)
+    data = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.produto_novo, self.produto_usado
+
